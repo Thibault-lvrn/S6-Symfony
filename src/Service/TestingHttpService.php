704c3e1c63ecfc20fee05f2be443d218123b16be
thibault livran <thibault.livran@dnd.fr>
2024-04-18 10:30:21 +0200

    make-services: Add TestHttpController and TestingHttpService classes

---
+++ src/Service/TestingHttpService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class TestingHttpService
+{
+    public function __construct(
+        private HttpClientInterface $client,
+    ) {
+    }
+
+    public function fetchGitHubInformation(): array
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://api.github.com/repos/symfony/symfony-docs'
+        );
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+      
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        
+	    $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        
+      	$content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+
+        return $content;
+    }
+}
